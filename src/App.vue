06c497a16c9af637df7731152f918e7c66202084
victor9743 <70709626+victor9743@users.noreply.github.com>
2021-08-16 16:53:51 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h3>{{ dataAtual }} / {{ mesAtual }} / {{ anoAtual }}</h3>
+  </div>
+</template>
+
+<script>
+let data = new Date();
+let dia = data.getDay();
+let mes = data.getMonth();
+let ano = data.getFullYear();
+if(dia <10){
+  dia = "0"+dia;
+}
+if(mes <10){
+  mes= "0"+mes;
+}
+export default{
+
+  data(){
+    return{
+        dataAtual: dia,
+        mesAtual: mes,
+        anoAtual: ano
+        
+    }
+    
+
+  }
+
+}
+
+</script>
+
+<style>
+
+
+</style>
